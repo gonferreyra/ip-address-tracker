a670eba89c24321f6029ce5327031bf8d4a15dd8
Gonzalo Ferreyra <gonferreyra@gmail.com>
2024-08-15 11:01:05 -0300

    custom icon added to the map

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -5,6 +5,7 @@ import { LatLngExpression, LatLngTuple } from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import 'leaflet-defaulticon-compatibility';
 import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css';
+import L from 'leaflet';
 
 type MapProps = {
   zoom?: number;
@@ -13,6 +14,17 @@ type MapProps = {
 
 export default function Map(props: MapProps) {
   const { zoom = 19, posix } = props;
+
+  const imageIcon = L.icon({
+    iconUrl: '/icon-location.svg',
+
+    iconSize: [40, 55], // size of the icon
+    // shadowSize:   [50, 64], // size of the shadow
+    iconAnchor: [22, 54], // point of the icon which will correspond to marker's location
+    // shadowAnchor: [4, 62],  // the same for the shadow
+    popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
+  });
+
   return (
     <MapContainer
       center={posix}
@@ -31,7 +43,7 @@ export default function Map(props: MapProps) {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
       />
-      <Marker position={posix}>
+      <Marker position={posix} icon={imageIcon}>
         <Popup>Your Location.</Popup>
       </Marker>
     </MapContainer>
