58cfefb7a4b3035a9f92e0009e4fae4cd7e11fa9
Gonzalo Ferreyra <gonferreyra@gmail.com>
2024-08-18 18:18:49 -0300

    skeleton color changed

--- src/components/skeleton.tsx
+++ src/components/skeleton.tsx
@@ -1,24 +1,24 @@
 export default function Skeleton() {
   return (
-    <div className='p-6 bg-white z-10 w-full max-w-md rounded-lg flex flex-col text-center gap-4 md:flex-row md:justify-between md:max-w-xl md:text-left anim'>
+    <div className='p-6 bg-white z-10 w-full max-w-md rounded-lg flex flex-col text-center gap-4 md:flex-row md:justify-between md:max-w-2xl md:text-left'>
       <div className='basis-1/4'>
-        <div className='animate-pulse w-full h-4 rounded-md bg-gray-300 mb-2' />
-        <div className='animate-pulse w-full h-4 rounded-md bg-gray-300' />
+        <div className='animate-pulse w-full h-4 rounded-md bg-gray-400 mb-2' />
+        <div className='animate-pulse w-full h-4 rounded-md bg-gray-400' />
       </div>
       <div className='border-l border-darkGray hidden md:block' />
       <div className='basis-1/4'>
-        <div className='animate-pulse w-full h-4 rounded-md bg-gray-300 mb-2' />
-        <div className='animate-pulse w-full h-4 rounded-md bg-gray-300' />
+        <div className='animate-pulse w-full h-4 rounded-md bg-gray-400 mb-2' />
+        <div className='animate-pulse w-full h-4 rounded-md bg-gray-400' />
       </div>
       <div className='border-l border-darkGray hidden md:block' />
       <div className='basis-1/4'>
-        <div className='animate-pulse w-full h-4 rounded-md bg-gray-300 mb-2' />
-        <div className='animate-pulse w-full h-4 rounded-md bg-gray-300' />
+        <div className='animate-pulse w-full h-4 rounded-md bg-gray-400 mb-2' />
+        <div className='animate-pulse w-full h-4 rounded-md bg-gray-400' />
       </div>
       <div className='border-l border-darkGray hidden md:block' />
       <div className='basis-1/4'>
-        <div className='animate-pulse w-full h-4 rounded-md bg-gray-300 mb-2' />
-        <div className='animate-pulse w-full h-4 rounded-md bg-gray-300' />
+        <div className='animate-pulse w-full h-4 rounded-md bg-gray-400 mb-2' />
+        <div className='animate-pulse w-full h-4 rounded-md bg-gray-400' />
       </div>
     </div>
   );
