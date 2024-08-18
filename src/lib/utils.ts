c537d0ac808b75355764236196d15f28fb518d72
Gonzalo Ferreyra <gonferreyra@gmail.com>
2024-08-18 18:19:11 -0300

    typo fixed on validateIp function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,4 @@
-export function ValidateIPaddress(ipaddress: string) {
+export function ValidateIPAddress(ipaddress: string) {
   if (
     /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(
       ipaddress
