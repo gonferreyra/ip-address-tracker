da1d8f7b5bdf1a94514985ee657ca673b58e200d
Gonzalo Ferreyra <gonferreyra@gmail.com>
2024-08-19 11:58:13 -0300

    handling undefined state

--- src/components/ip-information.tsx
+++ src/components/ip-information.tsx
@@ -12,31 +12,29 @@ export default function IpInformation() {
     return <Skeleton />;
   }
 
-  if (ipData) {
-    return (
-      <div className='p-6 bg-white z-10 w-full max-w-md rounded-lg flex flex-col text-center gap-4 md:flex-row md:justify-between md:max-w-2xl md:text-left'>
-        <div className='basis-1/4'>
-          <Title>IP Address</Title>
-          <ContentP>{ipData.ip}</ContentP>
-        </div>
-        <div className='border-l border-darkGray hidden md:block' />
-        <div className='basis-1/4'>
-          <Title>Location</Title>
-          <ContentP>
-            {ipData.location.region}, {ipData.location.country}
-          </ContentP>
-        </div>
-        <div className='border-l border-darkGray hidden md:block' />
-        <div className='basis-1/4'>
-          <Title>Timezone</Title>
-          <ContentP>UTC {ipData.location.timezone}</ContentP>
-        </div>
-        <div className='border-l border-darkGray hidden md:block' />
-        <div className='basis-1/4'>
-          <Title>ISP</Title>
-          <ContentP>{ipData.isp}</ContentP>
-        </div>
+  return (
+    <div className='p-6 bg-white z-10 w-full max-w-md rounded-lg flex flex-col text-center gap-4 md:flex-row md:justify-between md:max-w-2xl md:text-left'>
+      <div className='basis-1/4'>
+        <Title>IP Address</Title>
+        <ContentP>{ipData?.ip}</ContentP>
       </div>
-    );
-  }
+      <div className='border-l border-darkGray hidden md:block' />
+      <div className='basis-1/4'>
+        <Title>Location</Title>
+        <ContentP>
+          {ipData?.location.region}, {ipData?.location.country}
+        </ContentP>
+      </div>
+      <div className='border-l border-darkGray hidden md:block' />
+      <div className='basis-1/4'>
+        <Title>Timezone</Title>
+        <ContentP>UTC {ipData?.location.timezone}</ContentP>
+      </div>
+      <div className='border-l border-darkGray hidden md:block' />
+      <div className='basis-1/4'>
+        <Title>ISP</Title>
+        <ContentP>{ipData?.isp}</ContentP>
+      </div>
+    </div>
+  );
 }
--- src/components/map-container.tsx
+++ src/components/map-container.tsx
@@ -1,25 +1,29 @@
-import { countryTestData as testData } from '@/lib/db';
+'use client';
+
+import { useIpContext } from '@/lib/hooks';
 import dynamic from 'next/dynamic';
 import { useMemo } from 'react';
 
 export default function MapContainer() {
-  // console.log('map container rendered');
+  const { isLoading, ipData } = useIpContext();
+
   const Map = useMemo(
     () =>
       dynamic(() => import('@/components/map'), {
         loading: () => (
           <div className='flex justify-center items-center w-full h-full'>
-            Loading...
+            <div>Loading... </div>
           </div>
         ),
         ssr: false,
       }),
-    []
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [ipData]
   );
 
   return (
     <div className='z-0 h-[calc(100vh-210px)]'>
-      <Map posix={[testData.location.lat, testData.location.lng]} />
+      {ipData && <Map posix={[ipData.location.lat, ipData.location.lng]} />}
     </div>
   );
 }
